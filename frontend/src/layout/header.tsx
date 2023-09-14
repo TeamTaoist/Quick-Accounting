61a3ddf0914ec602c5cf74d7cafe340b8fe772c7
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-09-14 12:51:51 +0800

    add logo

--- frontend/src/layout/header.tsx
+++ frontend/src/layout/header.tsx
@@ -1,12 +1,15 @@
 import styled from "@emotion/styled";
 import { useAppContext } from "../provider/appProvider";
+import LogoIcon from "../assets/images/logo.png"
 
 export default function Header() {
   const { state: {account} } = useAppContext();
   return (
     <HeaderStyle>
       <nav>
-        <h1>Quick Accounting</h1>
+        <h1>
+          <img src={LogoIcon} alt="" />
+        </h1>
         {account && <span>{account}</span>}
       </nav>
     </HeaderStyle>
@@ -20,6 +23,10 @@ const HeaderStyle = styled.header`
   line-height: 80px;
   h1 {
     margin: 0;
+    img {
+      height: 60px;
+      margin-top: 10px;
+    }
   }
   nav {
     display: flex;
