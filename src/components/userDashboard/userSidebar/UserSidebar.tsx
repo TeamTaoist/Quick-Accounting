b1f994af54e39f753600ef0a3655661b6773ea52
CatFlow <22841916+Meow711@users.noreply.github.com>
2024-01-15 02:07:32 +0800

    add connect wallet

--- src/components/userDashboard/userSidebar/UserSidebar.tsx
+++ src/components/userDashboard/userSidebar/UserSidebar.tsx
@@ -3,9 +3,12 @@ import addressCard from "../../../assets/dashboard/address-card.svg";
 import file from "../../../assets/dashboard/file.svg";
 import arrow from "../../../assets/workspace/arrow.svg";
 import { useTranslation } from "react-i18next";
+import { useAccount } from "wagmi";
+import { getShortAddress } from "../../../utils/index";
 
 const UserSidebar = ({ children }: { children: React.ReactNode }) => {
   const { t } = useTranslation();
+  const { address } = useAccount();
   return (
     <>
       <div className="user-sidebar">
@@ -14,7 +17,7 @@ const UserSidebar = ({ children }: { children: React.ReactNode }) => {
         <div className="user-address">
           <div className="address">
             <img src={addressCard} alt="" />
-            <p>0x4dgf...2915</p>
+            <p>{getShortAddress(address || "")}</p>
           </div>
           <button className="disconnect">{t("user.Disconnect")}</button>
         </div>
--- src/pages/auth/login/LoginPopup.tsx
+++ src/pages/auth/login/LoginPopup.tsx
@@ -4,12 +4,64 @@ import "./loginPopup.scss";
 import cancelIcon from "../../../assets/auth/cancel.svg";
 import { Link, useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+import { useState, useEffect } from "react";
+import {
+  useConnect,
+  useAccount,
+  useSignMessage,
+  useChainId,
+  useDisconnect,
+  ConnectorAlreadyConnectedError,
+} from "wagmi";
 
 const LoginPopup = () => {
   const { t } = useTranslation();
   const navigate = useNavigate();
-  const handleLogin = () => {
-    navigate("/user");
+  const { connectors, isLoading: connectLoading, connectAsync } = useConnect();
+  const { isConnected, address } = useAccount();
+  const chainId = useChainId();
+  const { signMessageAsync, isLoading: signLoading } = useSignMessage();
+
+  const [clickConnectFlag, setClickConnectFlag] = useState(false);
+  const [loginLoading, setLoginLoading] = useState(false);
+
+  let loading = false;
+
+  useEffect(() => {
+    if (connectLoading || signLoading || loginLoading) {
+      if (!loading) {
+        // TODO show loading
+      }
+    } else if (loading) {
+      // TODO hide loading
+    }
+  }, [connectLoading, signLoading, loginLoading, loading]);
+
+  useEffect(() => {
+    if (isConnected && address && clickConnectFlag) {
+      // TODO sign
+      // TODO login
+      navigate("/user");
+    }
+  }, [isConnected, address, clickConnectFlag]);
+
+  const onClickConnect = async () => {
+    const connector = connectors[0];
+    if (!connector.ready) {
+      alert("Please install MetaMask");
+      return;
+    }
+    try {
+      // connect
+      await connectAsync({ connector });
+      setClickConnectFlag(true);
+    } catch (error) {
+      if (error instanceof ConnectorAlreadyConnectedError) {
+        if (isConnected && address) {
+          setClickConnectFlag(true);
+        }
+      }
+    }
   };
   return (
     <Header>
@@ -19,7 +71,7 @@ const LoginPopup = () => {
             <img className="close-btn" src={cancelIcon} alt="" />
           </Link>
           <h3>{t("login.LoginTitle")}</h3>
-          <button className="btn" onClick={handleLogin}>
+          <button className="btn" onClick={onClickConnect}>
             <img src={metamask} alt="" />
             <span>{t("login.MetaMask")}</span>
           </button>
