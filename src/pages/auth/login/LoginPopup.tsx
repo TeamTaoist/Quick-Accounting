0dd3590805d91b8e915e3d8aa43089e97ecee0ef
sshatil <mutasimshatil@hotmail.com>
2024-01-15 20:36:09 +0800

    design loading component

--- src/pages/auth/login/LoginPopup.tsx
+++ src/pages/auth/login/LoginPopup.tsx
@@ -4,9 +4,9 @@ import "./loginPopup.scss";
 import cancelIcon from "../../../assets/auth/cancel.svg";
 import { Link, useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import axiosClient from "../../../utils/axios";
 import { useAuthStore } from "../../../store/useAuthStore";
 import { useLoading } from "../../../store/useLoading";
+import Loading from "../../../utils/Loading";
 
 export interface LoginForm {
   domain: string;
@@ -24,19 +24,15 @@ const LoginPopup = () => {
     signature: "",
     wallet: "",
   };
-  // console.log(data.token);
   const { isLoading } = useLoading();
-  const { loginAsync, user } = useAuthStore();
+  const { loginAsync } = useAuthStore();
 
   const handleLogin = async () => {
     loginAsync(formValue, navigate);
-    // navigate("/user");
   };
-  console.log(user.token);
-
-  if (isLoading) return <p>Loading.....</p>;
   return (
     <Header>
+      {!isLoading && <Loading />}
       <div className="login">
         <div className="popup">
           <Link to="/">
---
+++ src/utils/Loading.tsx
@@ -0,0 +1,30 @@
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+import styled from "@emotion/styled";
+
+const Loading = () => {
+  return (
+    <LoadingContainer>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <CircularProgress />
+      </Box>
+    </LoadingContainer>
+  );
+};
+
+export default Loading;
+
+const LoadingContainer = styled.div`
+  background: rgba(10, 22, 11, 0.4);
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  z-index: 999;
+`;
