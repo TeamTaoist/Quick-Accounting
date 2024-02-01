a6f724b22cccbf761f3b77cde1611831eaa566ff
sshatil <mutasimshatil@hotmail.com>
2024-02-01 22:29:17 +0800

    fix payment request details attributes

--- src/pages/workspace/paymentRequest/PaymentRequestDetails.tsx
+++ src/pages/workspace/paymentRequest/PaymentRequestDetails.tsx
@@ -418,7 +418,7 @@ const PaymentRequestDetails = ({ setOpen }: PaymentRequestDetailsProps) => {
                     </TableCell>
                   </TableRow>
                   {categoryProperties?.map((property: any) => (
-                    <React.Fragment key={property.id}>
+                    <>
                       {property.type === "single-select" && (
                         <TableRow
                           sx={{
@@ -465,114 +465,110 @@ const PaymentRequestDetails = ({ setOpen }: PaymentRequestDetailsProps) => {
                           </TableCell>
                         </TableRow>
                       )}
-                      {
-                        <>
-                          {property.type === "multi-select" && (
-                            <TableRow
+                    </>
+                  ))}
+                  {categoryProperties?.map((property: any) => (
+                    <>
+                      {property.type === "multi-select" && (
+                        <TableRow
+                          sx={{
+                            td: {
+                              border: "1px solid var(--border-table)",
+                              padding: 1,
+                              paddingInline: 1,
+                            },
+                          }}
+                        >
+                          <TableCell sx={{ height: 1, width: 200 }}>
+                            <NoteInfo>
+                              <Image src={multiSelect} alt="" /> {property.name}
+                            </NoteInfo>
+                          </TableCell>
+
+                          <TableCell onBlur={handleUpdateCategory}>
+                            <ReactSelect
+                              isDisabled={paymentRequestDetails.status === 2}
+                              value={selectedValues}
+                              onChange={(
+                                selectedOptions: ReactSelectOption[]
+                              ) =>
+                                handleSelectChange(
+                                  selectedOptions,
+                                  property.name,
+                                  property.type
+                                )
+                              }
+                              options={property.values
+                                .split(";")
+                                .map((v: string) => ({
+                                  value: v,
+                                  label: v,
+                                }))}
+                              // defaultValues={property.values
+                              //   .split(";")
+                              //   .map((v: string) => ({
+                              //     value: v,
+                              //     label: v,
+                              //   }))}
+                              defaultValues={parseCategoryProperties
+                                .filter((p: any) => p.type === "multi-select")
+                                .map((p: any) =>
+                                  p.values.split(";").map((v: string) => ({
+                                    value: v,
+                                    label: v,
+                                  }))
+                                )
+                                .flat()}
+                            />
+                          </TableCell>
+                        </TableRow>
+                      )}
+                    </>
+                  ))}
+                  {categoryProperties?.map((property: any) => (
+                    <>
+                      {property.type === "Text" && (
+                        <TableRow
+                          sx={{
+                            td: {
+                              border: "1px solid var(--border-table)",
+                              padding: 1,
+                              paddingInline: 1,
+                            },
+                          }}
+                        >
+                          <TableCell sx={{ height: 1, width: 200 }}>
+                            <NoteInfo>
+                              <Image src={optionsIcon} alt="" /> {property.name}
+                            </NoteInfo>
+                          </TableCell>
+
+                          <TableCell onBlur={handleUpdateCategory}>
+                            <TextField
+                              disabled={paymentRequestDetails.status === 2}
                               sx={{
-                                td: {
-                                  border: "1px solid var(--border-table)",
-                                  padding: 1,
-                                  paddingInline: 1,
-                                },
+                                "& fieldset": { border: "none" },
                               }}
-                            >
-                              <TableCell sx={{ height: 1, width: 200 }}>
-                                <NoteInfo>
-                                  <Image src={multiSelect} alt="" />{" "}
-                                  {property.name}
-                                </NoteInfo>
-                              </TableCell>
-
-                              <TableCell onBlur={handleUpdateCategory}>
-                                <ReactSelect
-                                  isDisabled={
-                                    paymentRequestDetails.status === 2
-                                  }
-                                  value={selectedValues}
-                                  onChange={(
-                                    selectedOptions: ReactSelectOption[]
-                                  ) =>
-                                    handleSelectChange(
-                                      selectedOptions,
-                                      property.name,
-                                      property.type
-                                    )
-                                  }
-                                  options={property.values
-                                    .split(";")
-                                    .map((v: string) => ({
-                                      value: v,
-                                      label: v,
-                                    }))}
-                                  // defaultValues={property.values
-                                  //   .split(";")
-                                  //   .map((v: string) => ({
-                                  //     value: v,
-                                  //     label: v,
-                                  //   }))}
-                                  defaultValues={parseCategoryProperties
-                                    .filter(
-                                      (p: any) => p.type === "multi-select"
-                                    )
-                                    .map((p: any) =>
-                                      p.values.split(";").map((v: string) => ({
-                                        value: v,
-                                        label: v,
-                                      }))
-                                    )
-                                    .flat()}
-                                />
-                              </TableCell>
-                            </TableRow>
-                          )}
-                        </>
-                      }
-                      <>
-                        {property.type === "Text" && (
-                          <TableRow
-                            sx={{
-                              td: {
-                                border: "1px solid var(--border-table)",
-                                padding: 1,
-                                paddingInline: 1,
-                              },
-                            }}
-                          >
-                            <TableCell sx={{ height: 1, width: 200 }}>
-                              <NoteInfo>
-                                <Image src={optionsIcon} alt="" />{" "}
-                                {property.name}
-                              </NoteInfo>
-                            </TableCell>
-
-                            <TableCell onBlur={handleUpdateCategory}>
-                              <TextField
-                                disabled={paymentRequestDetails.status === 2}
-                                sx={{
-                                  "& fieldset": { border: "none" },
-                                }}
-                                size="small"
-                                fullWidth
-                                value={propertyContent}
-                                // id="fullWidth"
-                                placeholder="Enter content"
-                                onChange={(e) =>
-                                  handlePropertyText(
-                                    e,
-                                    property.name,
-                                    property.type
-                                  )
-                                }
-                                InputProps={{
-                                  style: { padding: 0 },
-                                }}
-                              />
-                            </TableCell>
-                          </TableRow>
-                        )}
-                      </>
-                    </React.Fragment>
+                              size="small"
+                              fullWidth
+                              value={propertyContent}
+                              // id="fullWidth"
+                              placeholder="Enter content"
+                              onChange={(e) =>
+                                handlePropertyText(
+                                  e,
+                                  property.name,
+                                  property.type
+                                )
+                              }
+                              InputProps={{
+                                style: { padding: 0 },
+                              }}
+                            />
+                          </TableCell>
+                        </TableRow>
+                      )}
+                    </>
                   ))}
                 </TableBody>
               </Table>
