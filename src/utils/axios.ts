df3d3a71f761cd0a36849324de5235d0d052ac58
sshatil <mutasimshatil@hotmail.com>
2024-02-05 18:23:26 +0800

    changed the server address

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: `https://quick-accounting-be.vercel.app`,
+  baseURL: `https://qa-api.taoist.dev`,
 });
 
 axiosClient.interceptors.request.use((config) => {
