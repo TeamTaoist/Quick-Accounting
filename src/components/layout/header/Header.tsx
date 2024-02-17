4d69f0d2578ce41e321affd2f02394ccdc2833c4
CatFlow <22841916+Meow711@users.noreply.github.com>
2024-02-17 09:00:36 +0800

    update version info display

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,10 +1,12 @@
 import Navbar from "../navbar/Navbar";
+import { VersionAtRight } from "../../version";
 
 const Header = ({ children, ...props }: { children: React.ReactNode }) => {
   return (
     <div {...props}>
       <Navbar />
       <div className="container">{children}</div>
+      <VersionAtRight />
     </div>
   );
 };
