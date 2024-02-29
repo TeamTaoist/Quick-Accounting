89081d139a4bb061f60ac704bd41edb35832ec91
sshatil <mutasimshatil@hotmail.com>
2024-02-29 12:12:50 +0800

    remove my payments extra scroll

--- src/pages/userDashboard/MyPayment.tsx
+++ src/pages/userDashboard/MyPayment.tsx
@@ -127,8 +127,6 @@ export const Details = styled.div`
   }
 `;
 export const PaymentTable = styled.div`
-  height: 100vh;
-  overflow-y: scroll;
   flex: 1;
 `;
 export const PaymentPagination = styled.div`
